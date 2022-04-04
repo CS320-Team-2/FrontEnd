9f6aefcc6a755657a0ce42a66b7a3a8d871bb918
ajeyrgowda <72326100+ajeyrgowda@users.noreply.github.com>
2022-04-04 14:56:47 -0400

    Reorganizing main page

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -2,13 +2,20 @@ import React from "react";
 import Navbar from "./Navbar";
 import Home from "./hpage";
 import RequestStatus from "./requeststatus";
+import "./main.css";
 
 function Main() {
     return(
         <div>
             <Navbar/>
-            <Home/>
-            <RequestStatus/>
+            <div className="CONT"> 
+                <div className="todolist">
+                <Home/>
+                </div>
+                <div className="reqstatus">
+                <RequestStatus/>
+                </div>
+            </div>
         </div>
     );
 }
