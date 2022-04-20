65341331498cf9aff88503c12e701b92f39a8e7e
njyothiraj <>
2022-04-20 14:57:25 -0400

    Logout Button redirects to login page

--- my-app/src/manager.js
+++ my-app/src/manager.js
@@ -28,7 +28,7 @@ function Manager() {
   {/* <a class ="button"><center></center></a> */}
   <div class="dropdown-content">
     <a href="#">32221178</a>
-    <a href="#">Logout</a>
+    <a href = 'http://localhost:3000/'> Logout </a>
     {/* <a href="#">Link 3</a> */}
   </div>
 </div>
