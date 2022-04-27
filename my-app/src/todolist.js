433c3a80fc70a5dc7a284701710e9047b6baf482
Daniel Barskiy <dbarskiy@umass.edu>
2022-04-27 14:03:39 -0400

    Added Task: Peer Eval and Requester:

--- my-app/src/todolist.js
+++ my-app/src/todolist.js
@@ -28,19 +28,25 @@ function toInProgress() {
 function ToDoList() {
   return (
     <div className="BigBox">
-      <div className="TitleBox">
-        <h1>To-Do List</h1>
-      </div>
-      <div className="IPtitlebox">
-        <h2>In-Progress Tasks:</h2>
-      </div>
-      <div className="IPlist">
-        <div className="IPitem">
-          <div class="scroll-area-sm">
-            <perfect-scrollbar class="ps-show-limits">
-              <div style={{ position: "static" }} class="ps ps--active-y">
-                <div class="ps-content">
-                  <li class="list-group-item-IP">
+    <div className="TitleBox">
+      <h1>To-Do List</h1>
+    </div>
+    <div className="IPtitlebox">
+      <h2>In-Progress Tasks:</h2>
+    </div>
+    <div className="IPlist">
+      <div className="IPitem">
+        <div class="scroll-area-sm">
+          <perfect-scrollbar class="ps-show-limits">
+            <div style={{ position: "static" }} class="ps ps--active-y">
+              <div class="ps-content">
+                <li class="list-group-item-IP">
+                  <div classname='TypeOfPage'>
+                    <h3>Task: Peer Eval</h3>
+                  </div>
+                  <div classname='Requester'>
+                    <h4>Requester: Sean Darras</h4>
+                  </div>
                     <div class="tododropdown">
                       <a class="todouser-button">Status</a>
                       <div class="tododropdown-content">
@@ -49,56 +55,55 @@ function ToDoList() {
                         <button onClick={tocomplete}>Completed</button>
                       </div>
                     </div>
-                    <div classname='typeOfTask'>
-                      <p>Task: </p>
-                    </div>
-                  </li>
-                  
-                </div>
+                 
+                </li>
+                
               </div>
-            </perfect-scrollbar>
-          </div>
-        </div>
-      </div>
-      <div className="NStitlebox">
-        <h2>Not Started Tasks:</h2>
-      </div>
-      <div className="NSlist">
-        <div className="NSitem">
-          <div class="scroll-area-sm">
-            <perfect-scrollbar class="ps-show-limits">
-              <div style={{ position: "static" }} class="ps ps--active-y">
-              <div class="ps-content">
-                  <li class="list-group-item-NS">
-                  
-                  </li>
-                  
-                </div>
-              </div>
-            </perfect-scrollbar>
-          </div>
-        </div>
-      </div>
-      <div className="Ctitlebox">
-        <h2>Completed Tasks:</h2>
-      </div>
-      <div className="Clist">
-        <div className="Citem">
-          <div class="scroll-area-sm">
-            <perfect-scrollbar class="ps-show-limits">
-              <div style={{ position: "static" }} class="ps ps--active-y">
-                <div class="ps-content">
-                  <li class="list-group-item-C">
-                  
-                  </li>
-                </div>
-              </div>
-            </perfect-scrollbar>
-          </div>
+            </div>
+          </perfect-scrollbar>
         </div>
       </div>
     </div>
-  );
+    <div className="NStitlebox">
+      <h2>Not Started Tasks:</h2>
+    </div>
+    <div className="NSlist">
+      <div className="NSitem">
+        <div class="scroll-area-sm">
+          <perfect-scrollbar class="ps-show-limits">
+            <div style={{ position: "static" }} class="ps ps--active-y">
+            <div class="ps-content">
+                <li class="list-group-item-NS">
+                
+                </li>
+                
+              </div>
+            </div>
+          </perfect-scrollbar>
+        </div>
+      </div>
+    </div>
+    <div className="Ctitlebox">
+      <h2>Completed Tasks:</h2>
+    </div>
+    <div className="Clist">
+      <div className="Citem">
+        <div class="scroll-area-sm">
+          <perfect-scrollbar class="ps-show-limits">
+            <div style={{ position: "static" }} class="ps ps--active-y">
+              <div class="ps-content">
+                <li class="list-group-item-C">
+                
+                </li>
+                
+              </div>
+            </div>
+          </perfect-scrollbar>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 }
 
 export default ToDoList;
