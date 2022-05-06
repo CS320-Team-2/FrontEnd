29812ae05a449ebf0a8eb048137975c3a40a69b5
miao tingshuo <miaoti@students.santiam.org>
2022-05-06 12:22:44 -0400

    Update todolist.js

--- my-app/src/todolist.js
+++ my-app/src/todolist.js
@@ -11,6 +11,7 @@ import "./todolist.css";
 
 function ToDoList() {
   const [todolist,settodolist] = useState([]);
+  const [performance,setperformanceData] = useState([]);
   let web = document.location.href;
   const te = web.split("/");
   let manager = te[4];
@@ -20,8 +21,36 @@ function ToDoList() {
   useEffect(() => {
     // TODO: Call Database API to get database info
     getTODO();
+    getPerformance();
   }, []);
   
+  function getPerformance(){
+    let url = 'http://localhost:3000/performance/';
+
+    fetch(url)//
+      .then(response => {
+          if (response.ok) {
+            console.log('it runs');
+            return response.json();
+          } else {
+              console.log('doesn t fetch')
+              throw new Error('Something went wrong ...');
+          }
+        })
+       .then(data =>{
+           let temp = [];
+          for (let i in data.list) {
+            if(data.list[i].to_employee == num_id){
+              let dataTemp = [data.list[i].id,data.list[i].from_employee,data.list[i].status];
+              //0.id,1.from_employee,2.status
+              temp.push(dataTemp);
+              }
+            }
+              setperformanceData(temp);
+          }
+        );
+        
+  }
   
   function getTODO(){
     let url = 'http://localhost:3000/assignedtraining/'+ab;
@@ -60,6 +89,52 @@ function ToDoList() {
       }
     }).then(response => {
       getTODO();
+    })
+  }
+
+  function tocomplete_review(e) {
+    let id = e.target.value;
+    fetch(' http://localhost:3000/performance/', {
+      method: 'PUT',
+      body: JSON.stringify({
+        id: id,
+        status: 2
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    }).then(response => {
+      getPerformance();
+    })
+  }
+  function toInProgress_review(e) {
+    let id = e.target.value;
+    fetch(' http://localhost:3000/performance/', {
+      method: 'PUT',
+      body: JSON.stringify({
+        id: id,
+        status: 1
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    }).then(response => {
+      getPerformance();
+    })
+  }
+  function toNotStarted_review(e) {
+    let id = e.target.value;
+    fetch(' http://localhost:3000/performance/', {
+      method: 'PUT',
+      body: JSON.stringify({
+        id: id,
+        status: 0
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    }).then(response => {
+      getPerformance();
     })
   }
 
@@ -134,6 +209,33 @@ function ToDoList() {
                   )
                 }})}
 
+                {performance.map((element,index)=>{
+                  //0.id,1.from_employee,2.status
+                  if(element[2]==1){
+               return(
+                 <>
+               <li class="list-group-item-IP">
+                 <div classname='TypeOfPage'>
+                 <todoh3> You are writting the peer review for the employee with id: "{element[1]}. After writting, please change the status as Completed.</todoh3>
+                 </div>
+
+                 
+                 <div class="tododropdown">
+                   <a class="todouser-button">Status</a>
+                   <div class="tododropdown-content">
+                     <button type="button" class="IPbutton" value={element[0]} onClick={toInProgress_review}>In-Progress</button>
+                     <button type="button" class="NSbutton" value={element[0]} onClick={toNotStarted_review}>Not Started</button>
+                     <button type="button" class="Cbutton" value={element[0]} onClick={tocomplete_review}>Completed</button>
+                   </div>
+                 </div>
+               </li>
+               </>
+               )
+                }})
+               }  
+
+
+
 
                 </div>
               </div>
@@ -174,6 +276,30 @@ function ToDoList() {
                   </>
                   )
                 }})}
+                {performance.map((element,index)=>{
+                  //0.id,1.from_employee,2.status
+                  if(element[2]==0){
+               return(
+                 <>
+               <li class="list-group-item-NS">
+                 <div classname='TypeOfPage'>
+                 <todoh3> Peer Review Request from {element[1]}. You can write review by click the button on the top of the page. </todoh3>
+                 </div>
+
+                 
+                 <div class="tododropdown">
+                   <a class="todouser-button">Status</a>
+                   <div class="tododropdown-content">
+                     <button type="button" class="IPbutton" value={element[0]} onClick={toInProgress_review}>In-Progress</button>
+                     <button type="button" class="NSbutton" value={element[0]} onClick={toNotStarted_review}>Not Started</button>
+                     <button type="button" class="Cbutton" value={element[0]} onClick={tocomplete_review}>Completed</button>
+                   </div>
+                 </div>
+               </li>
+               </>
+               )
+                }})
+               }
 
                 </div>
               </div>
@@ -213,7 +339,32 @@ function ToDoList() {
                   </li>
                   </>
                   )
-                  }})}
+                  }})
+                  }
+                   {performance.map((element,index)=>{
+                     //0.id,1.from_employee,2.status
+                     if(element[2]==2){
+                  return(
+                    <>
+                  <li class="list-group-item-C">
+                    <div classname='TypeOfPage'>
+                    <todoh3> You have finished the Peer Review form employee with id "{element[1]}". Thank you so much!</todoh3>
+                    </div>
+
+                    
+                    <div class="tododropdown">
+                      <a class="todouser-button">Status</a>
+                      <div class="tododropdown-content">
+                        <button type="button" class="IPbutton" value={element[0]} onClick={toInProgress_review}>In-Progress</button>
+                        <button type="button" class="NSbutton" value={element[0]} onClick={toNotStarted_review}>Not Started</button>
+                        <button type="button" class="Cbutton" value={element[0]} onClick={tocomplete_review}>Completed</button>
+                      </div>
+                    </div>
+                  </li>
+                  </>
+                  )
+                   }})
+                  }
 
                 </div>
               </div>
